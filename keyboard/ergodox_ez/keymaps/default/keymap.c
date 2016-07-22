5384938eb54817989483621a376e43460404de68
hayato-SMZ <aegis.tkx@gmail.com>
2016-07-22 16:35:47 +0900

    Update keymap.c

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
        KC_TRNS, KC_MUTE,
        KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_LCTL
+       KC_TRNS, KC_TRNS, KC_RCTL
     ),
 
 /* Keymap 2: Mouse Layer
