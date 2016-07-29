b032c275e45a46548f842b7accbd98bf5dfaa830
hayato-SMZ <aegis.tkx@gmail.com>
2016-07-29 14:59:20 +0900

    Update keymap.c

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|TOGGLE|           |CTLALT|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |MOUSE |           | DEL  |   N  |   M  |  ,<  |  .>  |   /  |   [{   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  cut | paste| copy | PgUp |  Up  |                                       | Down | PgDn |  [{  |  }]  |   \  |
+ *   | CTRL | Alt  | copy | PgUp |  Up  |                                       | Down | PgDn |  [{  |  }]  |   \  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | LGui | LAlt |       | Alt  | LFn  |
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   MO(FN),
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   TG(MOUSE),
-        LCTL(KC_X),     LCTL(KC_V),   LCTL(KC_C),   KC_PGUP,  KC_UP,
+        KC_RCTL,        KC_LALT,      LCTL(KC_C),   KC_PGUP,  KC_UP,
                                                 KC_LGUI,        KC_LALT,
                                                                 KC_HOME,
                                                 KC_SPC,KC_BSPC, KC_LEFT,
