f52429aa6f2890c2fc7f860b64c0e51c5d84f04d
hayato-SMZ <aegis.tkx@gmail.com>
2017-02-12 21:41:28 +0900

    Update keymap.c
    INT_2->KANA

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 KC_HOME,
                                                 KC_SPC,KC_BSLS, KC_LEFT,
         // right hand
-        KC_INT_2,            KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+        KC_KANA_2,            KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
         MO(FN),             KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                             KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
         LALT(LCTL(KC_DEL)) ,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_EQL,
