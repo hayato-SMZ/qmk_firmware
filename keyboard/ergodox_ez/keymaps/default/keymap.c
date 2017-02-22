b56852582c468fa3256bb37a2460d3d4b39485c5
hayato-SMZ <aegis.tkx@gmail.com>
2017-02-22 10:06:36 +0900

    Update keymap.c

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(FN),             KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                             KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
         LALT(LCTL(KC_DEL)) ,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_BSPC,
-                                   KC_DOWN, KC_PGDN ,KC_EQL,KC_RBRC,          ,
+                                   KC_DOWN, KC_PGDN ,KC_EQL,KC_RBRC,          KC_RO,
              KC_LALT,        MO(FN),
              KC_END,
              KC_RGHT,LALT(KC_ESC), KC_ENT
