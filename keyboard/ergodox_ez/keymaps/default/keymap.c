a6ed10ab20e63814b7180ba1cfd16575343efbd3
hayato-SMZ <aegis.tkx@gmail.com>
2017-03-10 14:08:43 +0900

    Update keymap.c
    イコールの位置の修正

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -15,9 +15,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | ESC    |   1  |   2  |   3  |   4  |   5  | PSCR |           |  \|  |   6  |   7  |   8  |   9  |   0  |   -_    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | LFn  |           | LFn  |   Y  |   U  |   I  |   O  |   P  |   =+   |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | LFn  |           | LFn  |   Y  |   U  |   I  |   O  |   P  |   '"   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | CTRL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   '"   |
+ * | CTRL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   =+   |
  * |--------+------+------+------+------+------|TOGGLE|           |CTLALT|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |MOUSE |           | DEL  |   N  |   M  |  ,<  |  .>  |   /?  |   ^~   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 KC_SPC,KC_BSLS, KC_LEFT,
         // right hand
         KC_BSPC ,            KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-        MO(FN),             KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_EQL,
-                            KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
+        MO(FN),             KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_QUOT,
+                            KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_EQL,
         LALT(LCTL(KC_DEL)) ,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_ESC,
                                    KC_DOWN, KC_PGDN ,KC_LBRC,KC_RBRC,          KC_F11,
              KC_LALT,        MO(FN),
