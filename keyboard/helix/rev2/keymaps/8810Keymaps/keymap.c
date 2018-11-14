78d78ecfc7c708437b37538e0af4dee2079a130d
hayato-SMZ <aegis.tkx@gmail.com>
2018-11-14 22:31:15 +0900

    chenge keymap

--- keyboard/helix/rev2/keymaps/8810Keymaps/keymap.c
+++ keyboard/helix/rev2/keymaps/8810Keymaps/keymap.c
@@ -62,23 +62,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  -   |
+   * | ESC  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  -   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  =   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |Enter |
+   * |Adjusy|   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |Lower |Raise |   N  |   M  |   ,  |   .  |   /  |  -   |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |Lower |Raise |   N  |   M  |   ,  |   .  |   /  |  \   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust| RCtrl| Alt  | GUI  | EISU | EISU |Space | Bksp | KANA |   '  |   [  |   ]  |   `  |  |   |
+   * | Ctrl | Alt  |  GUI | LEFT |  Up  |Space |Bksp  |KANA  |Enter | DOWN | Right|  [   |   ]  |   `  |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL , \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT , \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER ,   RAISE , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS , \
-      ADJUST, KC_RCTL,  KC_LALT, KC_LGUI, EISU,    EISU,   KC_SPC,  KC_BSPC, KANA ,   KC_QUOT,  KC_LBRC, KC_RBRC, KC_GRV,   KC_PIPE \
+      KC_RCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT , \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER ,   RAISE,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
+      ADJUST,  KC_LALT, KC_LGUI, KC_LEFT, KC_UP,   KC_SPC,  KC_BSPC,  KANA,   KC_ENT,  KC_DOWN,   KC_RGHT, KC_LBRC, KC_RBRC, KC_GRV  \
       ),
 
   /* Colemak
@@ -169,21 +169,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      | Reset|RGBRST|      |      |      |             |      |      |      |      |      |  Del |
+   * |      | CTL+Q| CTL+W| CTL+E|Reset |RGBRST|             | CTL+Y|      |      |      |      |  Del |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |Aud on|Audoff| Mac  |             | Win  |Qwerty|Colemk|Dvorak|      |      |
+   * |      | CTL+A| CTL+S|Aud on|Audoff| Mac  |             | Win  |Qwerty|      |      |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      |RGB ON| HUE+ | SAT+ | VAL+ |
+   * |      | CTL+Z| CTL+X  CTL+C| CTL+V|      |      |      | CTL+N|      |RGB ON| HUE+ | SAT+ | VAL+ |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      | LEFT |  Up  | DOWN | Right |      | MODE | HUE- | SAT- | VAL- |
+   * |      | DEL  |  Alt | Ctrl |  UP  | LEFT | DEL  | EISU |Right | DOWN | MODE | HUE- | SAT- | VAL- |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_ADJUST] =  LAYOUT( \
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-      _______, RESET,   RGBRST,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL, \
-      _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
-      _______, _______, _______, _______, _______, KC_LEFT,  KC_UP , KC_DOWN, KC_RGHT, _______, RGB_SMOD,RGB_HUD, RGB_SAD, RGB_VAD \
+      _______, LCTL(KC_Q), LCTL(KC_W),LCTL(KC_E), RESET, RGBRST,                   LCTL(KC_Y), _______, _______, _______, _______, KC_DEL, \
+      _______, LCTL(KC_A), LCTL(KC_S),AU_ON,AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  _______, _______,  _______, _______, \
+      _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______, _______, _______, LCTL(KC_N), _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
+      _______, KC_DEL,  KC_LALT, KC_LCTL, KC_UP, KC_LEFT, KC_DEL, EISU, KC_RGHT, KC_DOWN,  RGB_SMOD,RGB_HUD, RGB_SAD, RGB_VAD \
       )
 };
 
